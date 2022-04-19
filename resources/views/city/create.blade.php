baad82cfb1aab6d9deaa45db746b27451ff5fa0a
Otaviopax <83987042+Otaviopax@users.noreply.github.com>
2022-04-19 16:54:09 -0300

    Views Cidade
    Views da Cidade incompleto

--- resources/views/city/create.blade.php
+++ resources/views/city/create.blade.php
@@ -1,12 +1,12 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-12">
+                <h1>
+                    Cidade - Cadastro
+                </h1>
                 <div class="card card-primary">
-                    <h1>
-                        Cidade - Cadastro
-                    </h1>
 
                     @if ($errors->any())
                         <div class="alert alert-danger">
---
+++ resources/views/city/show.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::page')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card card-primary">
+                    <h1>Cidade - Visualizar</h1>
+                    <div class="card-body">
+                        <form role="form"></form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
