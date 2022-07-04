d6c1f109bef1599f9af3962b313cb6b74a8504de
viniggj2005 <102488557+viniggj2005@users.noreply.github.com>
2022-07-04 18:28:10 -0300

    correção

--- resources/views/typical_food/edit.blade.php
+++ resources/views/typical_food/edit.blade.php
@@ -9,9 +9,25 @@
     <script src="/js/iziToast.min.js" type="text/javascript"></script>
     <script src="/js/jquery.form.min.js" type="text/javascript"></script>
     <script src="/js/formAjaxAlterar.js" type="text/javascript"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
     <script>
         $(document).ready(function(){
             $(".select2").select2();
+        })
+        $('#description').summernote({
+            placeholder: 'Hello stand alone ui',
+            tabsize: 2,
+            height: 120,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
         })
     </script>
 @endpush
