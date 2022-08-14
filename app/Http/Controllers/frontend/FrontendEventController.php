6d97d498520347ada78dc108db46d03561beca87
BiancaKampmann <col.bianca@uniuv.edu.br>
2022-08-14 09:48:11 -0300

    Update FrontendEventController.php

--- app/Http/Controllers/frontend/FrontendEventController.php
+++ app/Http/Controllers/frontend/FrontendEventController.php
@@ -14,9 +14,8 @@ class FrontendEventController extends Controller
     function index()
     {
         $events = Event::where('status', 'Aprovado')->whereDate('final_date', '>=', date('Y-m-d'))->orderBy('start_date')->take(3)->with('city', 'firstImage')->get();
-        //$category = Category::where('$event_category->category_id == $category->id');
         //return 'eventos - listagem';
-        return view('web.event.event_page', compact('events'));//,'category'//));
+        return view('web.event.event_page', compact('events'));
     }
 
     function show(int $id)
