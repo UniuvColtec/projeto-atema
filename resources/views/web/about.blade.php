ea920640072af1e3587e5ebaaf81a9e596471e90
BiancaKampmann <col.bianca@uniuv.edu.br>
2022-08-27 17:00:43 -0300

    Páginas "contato" e "quem somos"
                   AS PÁGINAS SÃO PROVISÓRIAS

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -3,5 +3,23 @@
 
 @section('content')
 
-    Sobre nós
+
+    <div class="container-md" style="background: var(--ci-color-green)">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <br>
+                <h2 style="color: #ffffff; text-align: center">ATEMA</h2>
+            </div>
+                <p style="text-align: justify; color:#ffffff" class="my-4">
+                        A Associação de Turismo e Meio Ambiente do Vale do Iguaçu – ATEMA, fundada aos 11 de julho de 2014 é uma associação de direito privado, sem fins lucrativos, com sede à Rua Dom Pedro II, n.º 303, centro, na cidade de União da Vitória – PR.<br>
+                    <br>A ATEMA tem por finalidade precípua:<br>
+                    <br> a) Contribuir para a evolução de políticas públicas em geral, com ênfase nas áreas do turismo e meio ambiente, agricultura orgânica, apicultura, agroindústria, agro-floresta e o “Pagamento por Serviços Ambientais”, cultura e patrimônio histórico, voltadas ao desenvolvimento sustentável.<br>
+                    <br>b) Trabalhar, gerir, estabelecer por todos os meios lícitos e disponíveis em benefício do turismo e do meio ambiente, como uma alternativa ao desenvolvimento aos municípios atendidos pela ATEMA, pela potencialidade do patrimônio natural, histórico e cultural, e região.<br>
+                    <br> c) Servir de base institucional às comunidades a que se dedica visando o seu desenvolvimento.<br>
+                    <br> d) Estabelecer relações e estreita articulação com órgãos, empresas públicas ou privadas, autarquias e entidades da União, do Estado e com as municipalidades, bem como instituições do Terceiro Setor e outras entidades  ligadas ao turismo e ao meio ambiente tendo em vista somar esforços em torno dos mesmos objetivos.<br>
+                    <br> A ATEMA no ano de 2021 passou a ser a entidade responsável pela IGR – Instância de Governança Regional da 15ª Região Turística do Estado do Paraná – Região Sul do Paraná. A IGR tem o papel de coordenar o Programa de Regionalização do Turismo, em âmbito regional, como um espaço democrático de debates, proposições e encaminhamentos, em um novo conceito de gestão descentralizada, devendo ainda, assegurar que os interesses e propostas estabelecidas no âmbito municipal sejam respeitados e de fortalecer a parceria público-privado.
+                    As Instâncias de Governança Regionais são as responsáveis pela definição de prioridades, pela coordenação das decisões a serem tomadas, pelo planejamento e execução do processo de desenvolvimento do turismo na região turística. Devem participar, também, nas decisões políticas, econômicas e sociais no âmbito regional.</p>
+        </div>
+
+
 @stop
--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -3,5 +3,48 @@
 
 @section('content')
 
-    Contato
+    <div class="container-md" style="background: var(--ci-color-green)">
+        <div class="container py-5">
+            <div class="row row-cols-1 row-cols-lg-2">
+                <div class="container">
+                    <div style="text-align: center">
+                        <h4 class="text-white my-3 my-md-4 my-lg-5" >Telefone:<br>(42) 99122-9878</h4>
+                        <h4 class="text-white my-3 my-md-4 my-lg-5" >Email:<br>atematurismo@gmail.com</h4>
+                        <h4 class="text-white my-3 my-md-4 my-lg-5">Localização:<br>Rua Dom Pedro II, Nº 303, centro<br>União da Vitória - PR</h4>
+                        <script src='https://maps.google.com/maps/api/js?key=AIzaSyDv7YRz1WWPhkr6aim8wEm4WDPBdk81z54'></script>
+
+                        <div style="overflow: hidden;height: 280px;width: 100%;color: #0095eb">
+                            <div id="gmap_canvas" style="height: 280px;width: 100%;"></div>
+                            <style>#gmap_canvas img{max-width: none!important;background:none!important}</style>
+                        </div>
+
+                        <script type="text/javascript">
+                            function init_map(){var myOptions= {zoom:15,center:new google.maps.LatLng(-26.2324284,-51.0917618),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById
+                            ('gmap_canvas'), myOptions);marker = new google.maps.Maker({map: map,position: new google.maps.LatLng(-26.2324284,-51.0917618)});infowindow = new google.maps.InfoWindow({content: ' <img src="Site/assets/img/logoatema.png">'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindon.open(map,marker);}
+                            google.maps.event.addDomListener(window, 'load', init_map);
+                        </script>
+                    </div>
+                </div>
+                <div class="container-sm"style="background-color: #98b54d">
+                    <h1 class="u-text u-text-1" style="color: #ffffff; text-align: center
+">Entre em contato conosco!</h1>
+                    <form class="d-flex my-3 my-md-4 my-lg-5" role="search">
+                        <input class="form-control rounded-3 bg-gray" type="search" placeholder="Seu nome" aria-label="Search">
+
+                    </form>
+                    <form class="d-flex my-3 my-md-4 my-lg-5" role="search">
+                        <input class="form-control rounded-3 bg-gray" type="search" placeholder="Seu email" aria-label="Search">
+                    </form>
+                    <form class="d-flex my-3 my-md-4 my-lg-5" role="search">
+                        <textarea rows="10" cols="40" maxlength="500" class="form-control rounded-3 bg-gray" type="search" placeholder="Deixe seu comentário" aria-label="Search"></textarea>
+                    </form>
+                    <form class="d-flex my-3 my-md-4 my-lg-5" role="search">
+                        <button type="subimit" class="form-control rounded-3 bg-gray">Enviar</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 @stop
