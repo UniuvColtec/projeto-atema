4ed67a0a6ba0b78b91e984a3114a7de1901349c4
Otaviopax <83987042+Otaviopax@users.noreply.github.com>
2022-08-28 16:59:03 -0300

    Alterações no show do evento(frontend e backend))
    - Adicionada a galeria
    - Adicionado modal do mapa no frontend

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -2,10 +2,13 @@
 @section('title', 'Eventos - Cadastro')
 
 @push('css')
-@endpush
-
-@push('js')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+@endpush
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+@push('js')
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
     <script src="{{ asset('js/bs-stepper.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/formAjaxAlterar.js') }}" type="text/javascript"></script>
@@ -59,7 +62,7 @@
             })
         })
         function verify() {
-            if(document.getElementById('description').value == ''){
+            if (document.getElementById('description').value == '') {
                 alert("Descrição deve ser preenchida");
             } else {
                 stepper.next();
@@ -152,13 +155,46 @@
                                             <label for="description">Descrição:</label>
                                             <textarea name="description" id="description" class="form-control" placeholder="Descrição" required></textarea>
                                         </div>
-                                        <div class="form-group">
-                                            <label for="start_date">Data de início:</label>
-                                            <input type="datetime-local" name="start_date" id="start_date" class="form-control" placeholder="Data de início" required value="{{ $todays_date }}" min="{{ $todays_date }}">
+                                        <div class="row">
+                                        <div class="col-3">
+                                            <div class="form-group">
+                                                <label for="start_date">Data de início:</label>
+                                                <input type="datetime-local" name="start_date" id="start_date" class="form-control" placeholder="Data de início" required value="{{ $todays_date }}" min="{{ $todays_date }}">
+                                            </div>
                                         </div>
-                                        <div class="form-group">
-                                            <label for="final_date">Data de encerramento:</label>
-                                            <input type="datetime-local" name="final_date" id="final_date" class="form-control" placeholder="Data de encerramento" required value="{{ \Carbon\Carbon::now()->addDay()->format('Y-m-d\TH:i:s') }}" min="{{ $todays_date }}">
+                                        <div class="col-3">
+                                            <div class="form-group">
+                                                <label for="final_date">Data de encerramento:</label>
+                                                <input type="datetime-local" name="final_date" id="final_date" class="form-control" placeholder="Data de encerramento" required value="{{ \Carbon\Carbon::now()->addDay()->format('Y-m-d\TH:i:s') }}" min="{{ $todays_date }}">
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#mapModal" style="transform: translateY(82%)">
+                                                Datas de outros eventos
+                                            </button>
+                                        </div>
+                                        <div class="modal fade" id="mapModal" tabindex="-1" role="dialog" aria-labelledby="mapModalTitle" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="mapModalLongTitle">Datas</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div style="display: grid; grid-template-columns: 1fr 1fr;">
+                                                        @foreach($events as $event)
+                                                            <p>{{ $event->name }} - {{ $event->start_date }}</p>
+                                                        @endforeach
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                         </div>
                                     </div>
                                     <div class="card-footer">
