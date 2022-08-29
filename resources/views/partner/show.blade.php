6f9ee9c0ba9611d01a100e7d65394f9cd151fdab
viniggj2005 <102488557+viniggj2005@users.noreply.github.com>
2022-08-29 16:41:43 -0300

    pagina interna dos parceiros

--- resources/views/partner/show.blade.php
+++ resources/views/partner/show.blade.php
@@ -3,9 +3,34 @@
 
 @push('css')
     <link rel="stylesheet" href="/css/iziToast.min.css">
+    <style>
+        .btn-download-foto {
+            padding: 1px 5px;
+            font-size: 12px;
+            line-height: 1.5;
+            border-radius: 3px;
+            color: #333;
+            background-color: #fff;
+            border-color: #ccc;
+            text-decoration: none;
+        }
+        .gallery-item {
+            width: 100%;
+        }
+        .grid-gallery {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-column-gap: 10px;
+            /*border: solid dimgray 3px;
+            border-radius: 5px;*/
+        }
+    </style>
 @endpush
 
 @push('js')
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="/js/iziToast.min.js" type="text/javascript"></script>
     <script src="/js/jquery.form.min.js" type="text/javascript"></script>
     <script src="/js/formAjaxCadastrar.js" type="text/javascript"></script>
@@ -14,6 +39,7 @@
             $(".select2").select2();
         })
     </script>
+
 @endpush
 
 @section('content_header')
@@ -57,6 +83,38 @@
                                 <br>
                                 {!! $partner->renderMap($partner->latitude, $partner->longitude) !!}
                             </div>
+                            <button type="button" class="btn btn-primary "  data-toggle="modal" data-target="#imageModal">
+                                Galeria de Imagens
+                            </button>
+                            <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="imageModalLongTitle">Galeria de Imagens</h5>
+                                            <button type="button" class="close"  data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class='grid-gallery'>
+                                                @foreach($partner->images as $image)
+                                                    <div class='grid-gallery-item'>
+                                                        <a href='{{ asset('files/' . $image->image->address) }}' class='btn-download-foto'  download>
+                                                            <small><span class='fas fa-download'></span></small>
+                                                        </a>
+                                                        <br>
+                                                        <img src='{{ asset('files/' . $image->image->address) }}' data-src='{{ asset('files/' . $image->image->address) }}' class='img-responsive mklbItem gallery-item' data-gallery='myGallery'>
+                                                        <br>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary " data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="container-fluid">
                                 <div style="display: flex; flex-direction: row; justify-content: space-evenly;">
                                     <div>
@@ -84,4 +142,12 @@
             </div>
         </div>
     </div>
+
+
+    <script>
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myInput').trigger('focus')
+        })
+    </script>
+    </main>
 @endsection
--- resources/views/web/partner/show.blade.php
+++ resources/views/web/partner/show.blade.php
@@ -31,14 +31,13 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 @endpush
 @section('content')
-
-
     <div class="container my-5">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <h2>{{ $partner->name }}</h2>
+        <img src="{{$partner->getUrlLogo()}}" alt="{{$partner->title}}" class="img-thumbnail" style="max-width: 200px; max-height: 200px;">
+        <div class="col-12 col-md-6">
+            <h2 class="display-4">{{ $partner->name }}</h2>
 
-            </div>
+        </div>
+        <div class="row">
             <div class="col-12 col-md-6 d-flex flex-column flex-lg-row justify-content-between gap-2 gap-md-0 mt-3 mt-lg-0">
                 <div class="d-flex align-items-center gap-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#FFBB13" class="bi bi-map-fill"
@@ -101,6 +100,15 @@
                             <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
                         </svg>
                         {{ $partner->site }}
+                    </div>
+                </a>
+                <a href="{{ $partner->email }}" target="_blank" class="px-4">
+                    <div class="d-flex align-items-center gap-1 text-nowrap">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#98B54D"
+                             class="bi  bi-envelope" viewBox="0 0 16 16">
+                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                        </svg>
+                        {{ $partner->email }}
                     </div>
                 </a>
                 <div class="d-flex align-items-center gap-1 text-nowrap">
